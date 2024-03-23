e9c5669e182579964873c9d084293bd1ef601d57
Saeed Misaghian (SaM) <78544726+SaM-92@users.noreply.github.com>
2024-03-23 14:57:10 +0000

    create sql_agent with llm and db

--- subs/agent.py
+++ subs/agent.py
@@ -4,20 +4,41 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-db = connetc_to_irish_db()
-load_dotenv()
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+# Function to initialize the ChatOpenAI model
+def init_llm() -> ChatOpenAI:
+    """
+    Initialize and return a ChatOpenAI language model instance.
+
+    Returns:
+        ChatOpenAI: An instance of the ChatOpenAI class.
+    """
+    return ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 
-def sql_agent(llm_input, db, prompt):
+def sql_agent(prompt):
+    """
+    Creates and returns a SQL agent configured with a language model, database connection,
+    and the provided prompt template.
+
+    Args:
+        prompt: The prompt template to be used by the SQL agent.
+
+    Returns:
+        An instance of the configured SQL agent.
+    """
+    load_dotenv()
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+    # Initialize the language model and database connection
+    llm = init_llm()
+    db = connetc_to_irish_db()
 
     agent = create_sql_agent(
-        llm=llm_input,
+        llm=llm,
         db=db,
         prompt=prompt,
         verbose=True,
         agent_type="openai-tools",
     )
-
     return agent
